c50e83dcd6ebb926079f64ce56dcabfebfe025b8
Pavel Kulagin <kuzwiz@mail.ru>
2025-11-07 22:05:29 +0000

    [fix] corner case when a player cannot use a bonus to place a single elephant

--- modules/php/Actions/PlaceSingleElephant.php
+++ modules/php/Actions/PlaceSingleElephant.php
@@ -7,6 +7,7 @@ use Bga\Games\Tembo\Managers\Meeples;
 use Bga\Games\Tembo\Managers\Players;
 use Bga\Games\Tembo\Models\Action;
 use Bga\Games\Tembo\Models\Board;
+use Bga\Games\Tembo\Models\Player;
 
 class PlaceSingleElephant extends Action
 {
@@ -20,6 +21,16 @@ class PlaceSingleElephant extends Action
     $board = new Board();
     $ignoreRough = $this->getCtxArg('ignoreRough') ?? false;
     return ['options' => $board->getAllPossibleCoordsSingle($ignoreRough)];
+  }
+
+  public function isDoable(Player $player): bool
+  {
+    if (empty($this->getArgs()['options'])) {
+      $msg = clienttranslate('No space for a single elephant to place, ${player_name} cannot use this bonus');
+      Notifications::message($msg, ['player' => $player]);
+      return false;
+    }
+    return true;
   }
 
   public function actPlaceSingleElephant(int $x, int $y)
